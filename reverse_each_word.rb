4488a2af2cf4fd708e9e4dbb6c150dde43f70250
Gene Campbell III <gcampbell391@gmail.com>
2020-02-11 19:38:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(stringOne)
   arrayOne = []
 stringOne.split(" ").each {|s| arrayOne.push(s) }
-pp arrayOne.each {|s| pus.reverse }
-pp arrayOne[0].reverse 
+pp arrayOne.each {|s| puts s.reverse }
 end 
